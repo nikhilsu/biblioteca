6b3370f312f20f35ddd619f98b2e1a91b4927feb
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-03 18:09:09 +0530

    Added Spec for Displaying list of books

--- src/com/thoughtworks/librarysys/ConsoleDisplay.java
+++ src/com/thoughtworks/librarysys/ConsoleDisplay.java
@@ -1,13 +1,21 @@
 package com.thoughtworks.librarysys;
+
+import java.util.ArrayList;
+
 //Takes in a string and prints it on the console
 public class ConsoleDisplay {
     private String message;
+    private ArrayList<String> books;
 
-    public ConsoleDisplay(String message) {
+    public ConsoleDisplay(String message, ArrayList<String> books) {
         this.message = message;
+        this.books = books;
     }
 
     public void displayOnScreen() {
-        System.out.print(message);
+        System.out.println(message);
+        for(String book : books){
+            System.out.println(book);
+        }
     }
 }
--- test/com/thoughtworks/librarysys/ConsoleDisplayTest.java
+++ test/com/thoughtworks/librarysys/ConsoleDisplayTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -24,10 +25,24 @@ public class ConsoleDisplayTest {
     @Test
     public void shouldDisplayAStringThatIsPassedToTheConsoleDisplay() {
         String message = "Welcome";
-        ConsoleDisplay consoleDisplay = new ConsoleDisplay(message);
+        ConsoleDisplay consoleDisplay = new ConsoleDisplay(message, new ArrayList<String>());
 
         consoleDisplay.displayOnScreen();
 
-        assertEquals("Welcome", outputStream.toString());
+        assertEquals("Welcome\n", outputStream.toString());
+    }
+
+    @Test
+    public void shouldDisplayAListOfBooksAlongWithTheWelcomeMessage() {
+        ArrayList<String> books = new ArrayList<String>();
+        books.add("Harry Potter");
+        books.add("The Monk Who Sold His Ferrari");
+        books.add("Inferno");
+        books.add("Kite Runner");
+        ConsoleDisplay consoleDisplay = new ConsoleDisplay("Welcome", books);
+
+        consoleDisplay.displayOnScreen();
+
+        assertEquals("Welcome\nHarry Potter\nThe Monk Who Sold His Ferrari\nInferno\nKite Runner\n", outputStream.toString());
     }
 }
