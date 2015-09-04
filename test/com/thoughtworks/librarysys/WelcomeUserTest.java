980cb8ad6b14a51c299d384ed2aa557561f19a07
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-04 12:57:15 +0530

    Removed unused code, No other functionality added

--- test/com/thoughtworks/librarysys/WelcomeUserTest.java
+++ test/com/thoughtworks/librarysys/WelcomeUserTest.java
@@ -1,26 +1,10 @@
 package com.thoughtworks.librarysys;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
 public class WelcomeUserTest {
-    final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-    @Before
-    public void setUp() {
-        System.setOut(new PrintStream(outputStream));
-    }
-
-    @After
-    public void cleanUp() {
-        System.setOut(System.out);
-    }
 
     @Test
     public void shouldPrintAWelcomeMessageToGreetTheUser() {
