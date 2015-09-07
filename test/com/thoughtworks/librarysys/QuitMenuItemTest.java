cfdfdd29453335ce0659798f3e3024cb60a83893
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-08 01:30:54 +0530

    Added A Spec for QuitMenuItem class should exit application  when the perform operation method is called

--- test/com/thoughtworks/librarysys/QuitMenuItemTest.java
+++ test/com/thoughtworks/librarysys/QuitMenuItemTest.java
@@ -1,15 +1,28 @@
 package com.thoughtworks.librarysys;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 import static org.junit.Assert.*;
 
 public class QuitMenuItemTest {
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
     @Test
     public void shouldReturnTheStringExitingApplicationWhenTheToStringMethodIsCalled() {
         QuitMenuItem quitMenuItem = new QuitMenuItem();
 
         assertEquals("Exiting Application", quitMenuItem.toString());
+    }
+
+    @Test
+    public void shouldExitTheApplicationWhenThePerformOperationMethodIsCalled() {
+        QuitMenuItem quitMenuItem = new QuitMenuItem();
+
+        exit.expectSystemExit();
+        quitMenuItem.performOperation();
     }
 }
