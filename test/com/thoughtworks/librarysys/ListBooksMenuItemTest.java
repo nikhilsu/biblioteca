601a6ec78df094b8f57fe4e1d89387204637ecdd
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-08 01:24:39 +0530

    Added A Spec for ListBooksMenuItem class should return the slist of all the books present in the library when the toString method is called

--- test/com/thoughtworks/librarysys/ListBooksMenuItemTest.java
+++ test/com/thoughtworks/librarysys/ListBooksMenuItemTest.java
@@ -28,4 +28,24 @@ public class ListBooksMenuItemTest {
         assertEquals(testString, listBooksMenuItem.performOperation());
     }
 
+    @Test
+    public void shouldReturnTheListOfAllTheBooksInTheLibraryWhenToStringMethodIsInvoked() {
+        ArrayList<Book> listOfBooks = new ArrayList<Book>();
+        Book bookOne = new Book("Gone Girl", "Gillian Flynn", 2014);
+        Book bookTwo = new Book("Kite Runner", "Khaled Hossieni", 2003);
+        Book bookThree = new Book("Inferno", "Dan Brown", 2012);
+        listOfBooks.add(bookOne);
+        listOfBooks.add(bookTwo);
+        listOfBooks.add(bookThree);
+        Library library = new Library(listOfBooks);
+        ListBooksMenuItem listBooksMenuItem = new ListBooksMenuItem(library);
+
+        String testString = String.format("%-30s%-30s%-20s\n", "Name Of The Book", "Author", "Year Of Publication") +
+                String.format("%-30s%-30s%-20s\n", "Gone Girl", "Gillian Flynn", 2014) +
+                String.format("%-30s%-30s%-20s\n", "Kite Runner", "Khaled Hossieni", 2003) +
+                String.format("%-30s%-30s%-20s\n", "Inferno", "Dan Brown", 2012);
+
+        assertEquals(testString, listBooksMenuItem.toString());
+    }
+
 }
