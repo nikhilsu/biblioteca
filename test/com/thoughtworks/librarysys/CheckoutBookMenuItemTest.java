74d01c606936940d0311a636dcc363d47f1d1142
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-08 17:16:17 +0530

    Added Spec for CheckoutBookMenuItem should not checkout a book that is not from the library

--- test/com/thoughtworks/librarysys/CheckoutBookMenuItemTest.java
+++ test/com/thoughtworks/librarysys/CheckoutBookMenuItemTest.java
@@ -26,8 +26,24 @@ public class CheckoutBookMenuItemTest {
         CheckoutBookMenuItem checkoutBookMenuItem = new CheckoutBookMenuItem(library, consoleInput);
 
         assertEquals("Done", checkoutBookMenuItem.performOperation());
-
     }
 
+    @Test
+    public void shouldNotCheckOutABookFromTheLibraryIfItIsNotABookFromTheLibrary() {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("Head First Java\n".getBytes());
+        System.setIn(inputStream);
+        ConsoleInput consoleInput = new ConsoleInput(new Scanner(System.in));
+        ArrayList<Book> listOfBooks = new ArrayList<>();
+        Book bookOne = new Book("Gone Girl", "Gillian Flynn", 2014);
+        Book bookTwo = new Book("Kite Runner", "Khaled Hossieni", 2003);
+        Book bookThree = new Book("Inferno", "Dan Brown", 2012);
+        listOfBooks.add(bookOne);
+        listOfBooks.add(bookTwo);
+        listOfBooks.add(bookThree);
+        Library library = new Library(listOfBooks);
+        CheckoutBookMenuItem checkoutBookMenuItem = new CheckoutBookMenuItem(library, consoleInput);
 
+        assertEquals("Can't perform OP", checkoutBookMenuItem.performOperation());
+
+    }
 }
