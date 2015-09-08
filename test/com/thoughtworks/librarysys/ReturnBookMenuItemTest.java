569188747e49ffe5a77623cb20894eeb46419cca
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-08 23:03:43 +0530

    Added Spec to returnBookMenuItem Class should not return a book to the library that does not belong to it

--- test/com/thoughtworks/librarysys/ReturnBookMenuItemTest.java
+++ test/com/thoughtworks/librarysys/ReturnBookMenuItemTest.java
@@ -8,8 +8,9 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class ReturnBookMenuItemTest {
+
     @Test
-    public void shouldReturnABookFromTheUserToTheLibraryByInputtingTheNameOfTheBook() {
+    public void shouldReturnABookFromTheUserToTheLibraryByTheInputNameOfTheBook() {
         ByteArrayInputStream inputStream = new ByteArrayInputStream("Inferno\n".getBytes());
         System.setIn(inputStream);
         ArrayList<Book> listOfBooks = new ArrayList<Book>();
@@ -24,6 +25,25 @@ public class ReturnBookMenuItemTest {
         ReturnBookMenuItem returnBookMenuItem = new ReturnBookMenuItem(library);
 
         assertEquals("Done\n", returnBookMenuItem.performOperation());
+        System.setIn(System.in);
+    }
+
+    @Test
+    public void shouldNotReturnABookToTheLibraryThatDoesNotBelongToIt() {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("Head First Java\n".getBytes());
+        System.setIn(inputStream);
+        ArrayList<Book> listOfBooks = new ArrayList<Book>();
+        Book bookOne = new Book("Gone Girl", "Gillian Flynn", 2014);
+        Book bookTwo = new Book("Kite Runner", "Khaled Hossieni", 2003);
+        listOfBooks.add(bookOne);
+        listOfBooks.add(bookTwo);
+        ArrayList<Book> listOfCheckedOutBooks = new ArrayList<>();
+        Book book = new Book("Inferno", "Dan Brown", 2012);
+        listOfCheckedOutBooks.add(book);
+        Library library = new Library(listOfBooks, listOfCheckedOutBooks);
+        ReturnBookMenuItem returnBookMenuItem = new ReturnBookMenuItem(library);
+
+        assertEquals("Can't perform OP\n", returnBookMenuItem.performOperation());
         System.setIn(System.in);
     }
 }
