c10041f5e2315f9a158532bdc4818cb9e3b577a7
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-09 00:39:12 +0530

    Added Spec to BibiliotecaApplication to give a 'That is not a valid book to return' when the user attempts to return a book that does not belong to the library

--- test/com/thoughtworks/librarysys/BibliotecaApplicationTest.java
+++ test/com/thoughtworks/librarysys/BibliotecaApplicationTest.java
@@ -239,6 +239,42 @@ public class BibliotecaApplicationTest {
     }
 
     @Test
+    public void shouldNotReturnABookThatDoesNotBelongToTheLibraryWhenTheUserSelectsTheReturnMenuMenuItem() {
+        WelcomeUser welcomeUser = new WelcomeUser("Welcome to The Biblioteca");
+        Book bookOne = new Book("Kite Runner", "Khaled Hossieni", 2003);
+        Book bookTwo = new Book("Inferno", "Dan Brown", 2012);
+        ArrayList<Book> listOfBooks = new ArrayList<>();
+        listOfBooks.add(bookOne);
+        listOfBooks.add(bookTwo);
+        ArrayList<Book> listOfCheckedOutBooks = new ArrayList<>();
+        Book borrowedBook = new Book("Gone Girl", "Gillian Flynn", 2014);
+        listOfCheckedOutBooks.add(borrowedBook);
+        Library library = new Library(listOfBooks, listOfCheckedOutBooks);
+        ArrayList<String> listOfMenuItems = new ArrayList<>();
+        listOfMenuItems.add("1. List Books");
+        listOfMenuItems.add("2. Checkout Book");
+        listOfMenuItems.add("3. Return Book");
+        listOfMenuItems.add("4. Quit");
+        MainMenuItem mainMenuItem = new MainMenuItem(listOfMenuItems);
+        BibliotecaApplication bibliotecaApplication = new BibliotecaApplication(welcomeUser, mainMenuItem, library);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream("Head First Java\n".getBytes());
+        System.setIn(inputStream);
+        ConsoleInput c = mock(ConsoleInput.class);
+        when(c.inputFromUser()).thenReturn("3");
+        bibliotecaApplication.run(c);
+        System.setIn(System.in);
+
+        String testString = "Welcome to The Biblioteca\n" +
+                "1. List Books\n" +
+                "2. Checkout Book\n" +
+                "3. Return Book\n" +
+                "4. Quit\n" +
+                "That is not a valid book to return\n";
+
+        assertEquals(testString, outputStream.toString());
+    }
+
+    @Test
     public void shouldExitTheApplicationWhenTheUserSelectsTheExitMenuItem() {
         WelcomeUser welcomeUser = new WelcomeUser("Welcome to The Biblioteca");
         Book bookOne = new Book("Kite Runner", "Khaled Hossieni", 2003);
