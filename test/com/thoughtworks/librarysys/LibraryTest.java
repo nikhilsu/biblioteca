8d2075bf400a18bd962306d1bc4f8c3a6fb0278f
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-08 22:47:08 +0530

    Added a Spec for the library class should contain the returned book in its list of available books

--- test/com/thoughtworks/librarysys/LibraryTest.java
+++ test/com/thoughtworks/librarysys/LibraryTest.java
@@ -95,4 +95,25 @@ public class LibraryTest {
         assertFalse(library.toReturn(libraryBook));
     }
 
+    @Test
+    public void shouldContainTheBookWhichWasReturnedInTheListOfAvailableBooks() {
+        ArrayList<Book> listOfBooks = new ArrayList<Book>();
+        Book bookOne = new Book("Gone Girl", "Gillian Flynn", 2014);
+        Book bookTwo = new Book("Kite Runner", "Khaled Hossieni", 2003);
+        listOfBooks.add(bookOne);
+        listOfBooks.add(bookTwo);
+        ArrayList<Book> listOfCheckedOutBooks = new ArrayList<>();
+        Book borrowedBook = new Book("Inferno", "Dan Brown", 2012);
+        listOfCheckedOutBooks.add(borrowedBook);
+        Library library = new Library(listOfBooks, listOfCheckedOutBooks);
+        Book libraryBook = new Book("Inferno", "Author", 0);
+
+        library.toReturn(libraryBook);
+        String testString = String.format("%-30s%-30s%-20s\n", "Name Of The Book", "Author", "Year Of Publication") +
+                String.format("%-30s%-30s%-20s\n", "Gone Girl", "Gillian Flynn", 2014) +
+                String.format("%-30s%-30s%-20s\n", "Kite Runner", "Khaled Hossieni", 2003) +
+                String.format("%-30s%-30s%-20s\n", "Inferno", "Dan Brown", 2012);
+
+        assertEquals(testString, library.toString());
+    }
 }
