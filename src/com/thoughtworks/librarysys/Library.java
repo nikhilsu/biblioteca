79ee256be61a1b19312be52b1f27824f987efbbb
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-15 18:17:28 +0530

    Added Spec for library to checkout a movie passed to it

--- src/com/thoughtworks/librarysys/Library.java
+++ src/com/thoughtworks/librarysys/Library.java
@@ -2,13 +2,19 @@ package com.thoughtworks.librarysys;
 
 public class Library {
     private Books books;
+    private Movies movies;
 
-    public Library(Books books) {
+    public Library(Books books, Movies movies) {
         this.books = books;
+        this.movies = movies;
     }
 
     public void checkOut(Book bookToCheckOut) {
         books.checkOut(bookToCheckOut);
+    }
+
+    public void checkOut(Movie movieToCheckout) {
+        movies.checkOut(movieToCheckout);
     }
 }
 
--- test/com/thoughtworks/librarysys/LibraryTest.java
+++ test/com/thoughtworks/librarysys/LibraryTest.java
@@ -11,11 +11,22 @@ public class LibraryTest {
     @Test
     public void shouldCheckOutABookPassedToIt() {
         Books books = mock(Books.class);
-        Library library = new Library(books);
+        Movies movies = mock(Movies.class);
+        Library library = new Library(books, movies);
 
         Book bookToCheckOut = new Book("Book", "Author", 0);
         library.checkOut(bookToCheckOut);
 
         verify(books).checkOut(bookToCheckOut);
+    }
+
+    @Test
+    public void shouldCheckOutAMoviePassedToIt() {
+        Books books = mock(Books.class);
+        Movies movies = mock(Movies.class);
+        Library library = new Library(books, movies);
+
+        Movie movieToCheckOut = new Movie("Movie", 1000, "Director", 0);
+        library.checkOut(movieToCheckOut);
     }
 }
