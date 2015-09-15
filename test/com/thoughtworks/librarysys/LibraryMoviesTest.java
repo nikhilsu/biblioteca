f5ae9657db56b2234556e07fc8dd25b1cff435ba
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-15 15:41:50 +0530

    Added A Spec for LibraryMovies should not checkout a movie that belongs to the library

--- test/com/thoughtworks/librarysys/LibraryMoviesTest.java
+++ test/com/thoughtworks/librarysys/LibraryMoviesTest.java
@@ -45,4 +45,24 @@ public class LibraryMoviesTest {
 
         assertEquals(testString, libraryMovies.displayListOfDetails());
     }
+
+    @Test
+    public void shouldNotCheckOutAMovieThatIsNotPresentInTheLibrary() {
+        ArrayList<Movie> listOfMovies = new ArrayList<>();
+        Movie movieOne = new Movie("Gone Girl", 2014, "David Fincher", 8);
+        Movie movieTwo = new Movie("Matrix", 1999, "Andy Wachowski", 9);
+        listOfMovies.add(movieOne);
+        listOfMovies.add(movieTwo);
+        LibraryMovies libraryMovies = new LibraryMovies(listOfMovies) ;
+
+        Movie movieToCheckOut = new Movie("Movie", 0, "Director" ,0);
+        libraryMovies.checkOut(movieToCheckOut);
+        String testString = String.format("%085d\n", 0).replace("0", "-") +
+                String.format("%-25s%-20s%-30s%-20s\n", "Name Of The Movie", "Year Of Release", "Director", "Rating") +
+                String.format("%085d\n", 0).replace("0", "-") +
+                String.format("%-25s%-20s%-30s%-20s\n", "Gone Girl", 2014, "David Fincher", 8) +
+                String.format("%-25s%-20s%-30s%-20s\n", "Matrix", 1999, "Andy Wachowski", 9) + "\n";
+
+        assertEquals(testString, libraryMovies.displayListOfDetails());
+    }
 }
