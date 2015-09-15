ea33a1f9a11b3348c03616a9871a1df2207ae47b
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-15 19:18:40 +0530

    Added a checkoutMovieMenuItem class and a spec for it to check out a movie that is present in the library

---
+++ src/com/thoughtworks/librarysys/CheckoutMovieMenuItem.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.librarysys;
+
+public class CheckoutMovieMenuItem extends MenuItem {
+    private Library library;
+    private ConsoleView consoleView;
+
+    public CheckoutMovieMenuItem(Library library, ConsoleView consoleView) {
+        this.library = library;
+        this.consoleView = consoleView;
+    }
+
+    @Override
+    public void performOperation() {
+        consoleView.printOnConsole("Enter The Movie to checkout: ");
+        String nameOfTheMovieToCheckout = consoleView.inputFromUser();
+        Movie movieToCheckout = new Movie(nameOfTheMovieToCheckout, 0, "Not needed", 0);
+        library.checkOut(movieToCheckout);
+    }
+}
---
+++ test/com/thoughtworks/librarysys/CheckoutMovieMenuItemTest.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.librarysys;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class CheckoutMovieMenuItemTest {
+
+    @Test
+    public void shouldCheckoutAMovieFromTheListOfMoviesInTheLibraryByInputtingTheNameOfTheMovie() {
+        Library library = mock(Library.class);
+        ConsoleView consoleView = mock(ConsoleView.class);
+        when(consoleView.inputFromUser()).thenReturn("Gone Girl");
+        CheckoutMovieMenuItem checkoutMovieMenuItem = new CheckoutMovieMenuItem(library, consoleView);
+        Movie movieToCheckout = new Movie("Gone Girl", 0, "Not needed", 0);
+        checkoutMovieMenuItem.performOperation();
+
+        verify(consoleView).printOnConsole("Enter The Movie to checkout: ");
+        verify(library).checkOut(movieToCheckout);
+    }
+}
