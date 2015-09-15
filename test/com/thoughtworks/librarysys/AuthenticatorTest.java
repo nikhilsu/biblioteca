c5cb24d3e5ad35d37a1abc4714a2f6c239d094ac
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-15 23:14:27 +0530

    Added spec for Authenticator should not autheticate an unregistered user

--- test/com/thoughtworks/librarysys/AuthenticatorTest.java
+++ test/com/thoughtworks/librarysys/AuthenticatorTest.java
@@ -33,4 +33,16 @@ public class AuthenticatorTest {
 
         assertFalse(authenticator.authenticateUser(userToAuthenticate));
     }
+
+    @Test
+    public void shouldNotAuthenticateAnUnregisteredUser() {
+        User userOne = new User("111-1111", "password1");
+        User userTwo = new User("222-2222", "password2");
+        User userThree = new User("333-3333", "password3");
+        ArrayList<User> listOfRegisteredUsers = new ArrayList<>(Arrays.asList(userOne, userTwo, userThree));
+        Authenticator authenticator = new Authenticator(listOfRegisteredUsers);
+        User userToAuthenticate = new User("Not registered", "password1");
+
+        assertFalse(authenticator.authenticateUser(userToAuthenticate));
+    }
 }
