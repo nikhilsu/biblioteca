e7102c74fc1918ee5137ebfdb04f5aa281a006f6
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-17 22:26:53 +0530

    Added Spec for a parserfactory should return a parser with an appropriate map for a guest

--- test/com/thoughtworks/librarysys/InputParserFactoryTest.java
+++ test/com/thoughtworks/librarysys/InputParserFactoryTest.java
@@ -32,4 +32,24 @@ public class InputParserFactoryTest {
 
         assertEquals(inputParserFactory.manufacture(user).getClass(), inputParser.getClass());
     }
+
+    @Test
+    public void shouldReturnAParserWithAnAppropriateMapperForTheGuestUser(){
+        Library library = mock(Library.class);
+        ConsoleView consoleView = mock(ConsoleView.class);
+        Users users = mock(Users.class);
+        InputParserFactory inputParserFactory = new InputParserFactory(library, consoleView, users);
+        User user = mock(User.class);
+        when(user.getRole()).thenReturn("Guest");
+        HashMap<String, MenuOptions> mapper = new HashMap<>();
+        mapper.put("1", new ListBooksMenuItem(library, consoleView));
+        mapper.put("2", new ListMovieMenuItem(library, consoleView));
+        mapper.put("3", new CheckoutMovieMenuItem(library, consoleView));
+        mapper.put("4", new LoginMenuItem(users, consoleView));
+        mapper.put("5", new QuitMenuItem());
+
+        InputParser inputParser = new InputParser(consoleView, mapper);
+
+        assertEquals(inputParserFactory.manufacture(user).getClass(), inputParser.getClass());
+    }
 }
