2e8d2414c2b82be8fc2040089b907db7a9801a8f
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-17 21:51:30 +0530

    Added a spec for a parser should not be equal to null

--- src/com/thoughtworks/librarysys/InputParser.java
+++ src/com/thoughtworks/librarysys/InputParser.java
@@ -21,7 +21,7 @@ public class InputParser {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj.getClass() != this.getClass())
+        if (obj == null || obj.getClass() != this.getClass())
             return false;
         InputParser that = (InputParser) obj;
         if(this.consoleView.equals(that.consoleView) && this.mapper.equals(that.mapper))
